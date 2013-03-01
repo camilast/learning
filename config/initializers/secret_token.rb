03f282cc316393ee6bb54969d23240cd238239d0
camilast <camila.timoteo@mapabrasil.com.br>
2013-03-01 15:23:30 -0300

    Rails Structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learning::Application.config.secret_token = '9296fb75b6473a4ec6e33d7bf03da48d61b97e8dcc8a9c57ea78d9d71809abe3527cda7deab90e45ff2fc9293c5c6056d310633001220f1c8bedd71042390b15'
